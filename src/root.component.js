c113df4e4a6bf84fd29847c5f3b9f3e7093808ac
Divya Goel <goel.divya.srms@gmail.com>
2022-07-26 12:05:33 +0530

    navigated to push

--- src/root.component.js
+++ src/root.component.js
@@ -1,11 +1,14 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Redirect, Route, Routes, Navigate } from "react-router-dom";
 import Navbar from "./components/NavBar";
 
 export default function Root(props) {
   return (
     <Router>
       <Navbar />
+      <Routes>
+        <Route path="/" element={<Navigate replace to="/home" />}></Route>
+      </Routes>
     </Router>
   );
 }
